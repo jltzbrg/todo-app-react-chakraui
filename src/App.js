cc38073d986a18681e8405ba9e03a87fbf3dc35b
Julio Litzenberg <julio.litzenberg@gmail.com>
2021-03-05 01:04:08 +0100

    Creating Icons and Heading

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import { Heading } from '@chakra-ui/react';
-import { VStack } from '@chakra-ui/react';
+import { Heading, VStack, IconButton } from '@chakra-ui/react';
 import TodoList from './components/TodoList';
 import AddTodo from './components/AddTodo';
+import { FaSun, FaMoon } from 'react-icons/fa';
 
 function App() {
   return (
-    <VStack>
-      <Heading>Todo Application</Heading>
+    <VStack p={4}>
+      <IconButton
+        icon={<FaSun />}
+        isRound='true'
+        size='lg'
+        alignSelf='flex-end'
+      />
+      <Heading
+        mb='8'
+        fontWeight='extrabold'
+        size='2xl'
+        bgGradient='linear(to-r, pink.500, pink.300, blue.500)'
+        bgClip='text'>
+        Todo Application
+      </Heading>
     </VStack>
   );
 }
